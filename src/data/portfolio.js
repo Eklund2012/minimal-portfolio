8769d6482fd1a44d360bfc4b661b9d9df1cb36cb
Eklund2012 <david.eklund9@gmail.com>
2025-03-26 21:20:32 +0100

    portfolio section

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: "Python chess puzzles",
+        imgUrl: '/assets/chess.jpg',
+        stack: ["Python", "Pygame"],
+        link: "davideklund.com",
+    },
+    {
+        title: "Developer Portfolio",
+        imgUrl: "/assets/chess.jpg",
+        stack: ["React", "TailwindCSS", "Vite"],
+        link: "davideklund.com",
+    },
+    {
+        title: "Terminal website",
+        imgUrl: "/assets/chess.jpg",
+        stack: ["React", "TailwindCSS"],
+        link: "https://fkcodes.com",
+    },
+]
