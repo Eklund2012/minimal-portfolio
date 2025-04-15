bef08bed2a4c2f0b393113f0aa1521f375285266
Eklund2012 <david.eklund9@gmail.com>
2025-04-15 21:35:53 +0200

    added a project, img and in data file

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,16 +1,23 @@
 export default [
     {
-        title: "Python chess puzzles",
-        imgUrl: '/assets/chess_puzzles.png',
-        alt: "Chess puzzles project",
-        stack: ["Python", "Pygame"],
-        link: "https://github.com/Eklund2012/py-chess-puzzles",
-    },
-    {
         title: "Portfolio Website",
         imgUrl: "/assets/portfolio.png",
         alt: "Minimal portfolio project",
         stack: ["React", "TailwindCSS", "Vite", "JavaScript"],
         link: "https://github.com/Eklund2012/minimal-portfolio",
+    },
+    {
+        title: "Python chess puzzles",
+        imgUrl: '/assets/chess_puzzles.png',
+        alt: "Chess puzzles project",
+        stack: ["Python", "Pygame", "Stockfish"],
+        link: "https://github.com/Eklund2012/py-chess-puzzles",
+    },
+    {
+        title: "Python top down survival game",
+        imgUrl: '/assets/zombie_survival.png',
+        alt: "Chess puzzles project",
+        stack: ["Python", "Pygame"],
+        link: "https://github.com/Eklund2012/zombie-survival",
     },
 ]
